622628e701d5dfe0d8b416e13c3ce8ca98bf546d
Carlos Pinheiro <carlosfilhocontato2008@gmail.com>
2026-09-24 12:24:37 -0300

    Add files via upload

---
+++ secret-garden-firebase/firebase-config.js
@@ -0,0 +1,10 @@
+// Cole aqui a configuracao do seu aplicativo Web criada no Console do Firebase.
+export const firebaseConfig = {
+  apiKey: "AIzaSyBT0LCrqhtzsqibo0z_jQKo2ueSB8pkPXc",
+  authDomain: "secret-garden-6a44b.firebaseapp.com",
+  projectId: "secret-garden-6a44b",
+  storageBucket: "secret-garden-6a44b.firebasestorage.app",
+  messagingSenderId: "563387107643",
+  appId: "1:563387107643:web:df9d8488134328d30ccf1e",
+  measurementId: "G-4NXLL8QW5X"
+};
